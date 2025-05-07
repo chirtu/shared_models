009a5f550958456f0b081a8195f5720f30c949ff
chirtu <chitufatuma45@gmail.com>
2025-05-07 16:14:31 +0300

    product categories declared

---
+++ lib/agiza_product_categories.dart
@@ -0,0 +1,10 @@
+class AgizaProductsCategory {
+
+  String fertiliser = 'Fertilizer';
+  String seeds = 'Grain & Cereal Seeds';
+  String equipments = 'Farm Equipment & Machinery';
+  String pesticides = 'Pesticides & Insecticides';
+
+}
+
+AgizaProductsCategory agizaProductsCategory = AgizaProductsCategory();
